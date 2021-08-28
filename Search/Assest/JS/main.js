920d3c08e77ff765ce1299f7dd92d6629027fed1
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-08-28 18:50:57 +0430

    if li was clicked, delete the bg-selected class of all elements and add bg-selected class to the selected li

--- Search/Assest/JS/main.js
+++ Search/Assest/JS/main.js
@@ -1,3 +1,31 @@
 const search = sessionStorage.getItem('search')
 document.title = search + '- Use Me As Second IMDB'
-document.querySelector('#search-bar').value = search
+document.querySelector('#search-bar').placeholder = search
+
+const selectUl = document.querySelector('.filter-list')
+const selectLi = document.querySelectorAll('.lists')
+
+eventListener()
+function eventListener() {
+  selectUl.addEventListener('click', selectedBg)
+}
+
+
+function selectedBg(e) {
+  selectLi.forEach(element => {
+    element.classList.remove('bg-selected')
+  });
+
+  switch (e.target.tagName) {
+    case 'LI':
+      e.target.classList.add('bg-selected')
+      break;
+    case 'P':
+      e.target.parentElement.classList.add('bg-selected')
+      break;
+    case 'SPAN':
+      e.target.parentElement.classList.add('bg-selected')
+      break;
+
+  }
+}
