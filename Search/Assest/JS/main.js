f9e3e1c158bf519661bfcf46161765b5677eabab
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-08-29 17:31:48 +0430

    create 3 skleton style div and get deta from api and put inside variable

--- Search/Assest/JS/main.js
+++ Search/Assest/JS/main.js
@@ -1,31 +1,62 @@
-const search = sessionStorage.getItem('search')
-document.title = search + '- Use Me As Second IMDB'
-document.querySelector('#search-bar').placeholder = search
+const search = sessionStorage.getItem("search");
+document.title = search + "- Use Me As Second IMDB";
+document.querySelector("#search-bar").placeholder = search;
 
-const selectUl = document.querySelector('.filter-list')
-const selectLi = document.querySelectorAll('.lists')
+const selectUl = document.querySelector(".filter-list");
+const selectLi = document.querySelectorAll(".lists");
 
-eventListener()
+let movie = getFromAPI("movie");
+tv = getFromAPI("tv");
+collection = getFromAPI("collection");
+person = getFromAPI("person");
+collection = getFromAPI("collection");
+keyword = getFromAPI("keyword");
+
+eventListener();
 function eventListener() {
-  selectUl.addEventListener('click', selectedBg)
+  selectUl.addEventListener("click", selectedBg);
 }
 
-
 function selectedBg(e) {
-  selectLi.forEach(element => {
-    element.classList.remove('bg-selected')
+  selectLi.forEach((element) => {
+    element.classList.remove("bg-selected");
   });
 
   switch (e.target.tagName) {
-    case 'LI':
-      e.target.classList.add('bg-selected')
+    case "LI":
+      e.target.classList.add("bg-selected");
       break;
-    case 'P':
-      e.target.parentElement.classList.add('bg-selected')
+    case "P":
+    case "SPAN":
+      e.target.parentElement.classList.add("bg-selected");
       break;
-    case 'SPAN':
-      e.target.parentElement.classList.add('bg-selected')
-      break;
-
   }
+}
+
+createSkelton(3);
+function createSkelton(Number) {
+  const path = document.getElementById("content");
+  for (let i = 1; i <= Number; i++) {
+    path.innerHTML += `
+    <div class="content-row">
+      <div class="content-row-img">
+        <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="">
+      </div>
+      <div class="content-row-text">
+        <h2 class="nothing1"></h2>
+        <p class="nothing2"></p>
+        <span class="nothing3"></span>
+      </div>
+    </div>
+    `;
+  }
+}
+
+async function getFromAPI(searchForWhat) {
+  const API = `https://api.themoviedb.org/3/search/${searchForWhat}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&query=lucifer&page=1&include_adult=false`;
+
+  const response = await fetch(API);
+  const result = await response.json();
+
+  return result;
 }
