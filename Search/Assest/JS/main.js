a7c6db1c20a03ccc42859ed6580a6fcb669ef6c2
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-08-31 19:03:40 +0430

    if click on every member of search result, background was change

--- Search/Assest/JS/main.js
+++ Search/Assest/JS/main.js
@@ -1,3 +1,26 @@
 const search = sessionStorage.getItem("search");
 document.title = search + "- Use Me As Second IMDB";
 document.querySelector("#search-bar").placeholder = search;
+
+const UL = document.querySelector(".filter-list");
+const LI = document.querySelectorAll(".lists");
+
+document.addEventListener("DOMContentLoaded", () => {
+  UL.addEventListener("click", selectedBg);
+});
+
+function selectedBg(e) {
+  LI.forEach((element) => {
+    element.classList.remove("bg-selected");
+  });
+
+  switch (e.target.tagName) {
+    case "LI":
+      e.target.classList.add("bg-selected");
+      break;
+    case "P":
+    case "SPAN":
+      e.target.parentElement.classList.add("bg-selected");
+      break;
+  }
+}
