489e690ddc47397757bb482c4f14b5429124c8d6
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-09-02 17:44:50 +0430

    get data from api, and set backdrop image for header background

---
+++ Movie/Assest/JS/main.js
@@ -0,0 +1,27 @@
+// const search = JSON.parse(sessionStorage.getItem("movie"));
+// document.title = search[search.length - 1] + "- Use Me As Second IMDB";
+// document.querySelector("#search-bar").placeholder = search[search.length - 1];
+// const querySearch = search[search.length - 1]
+
+const bgURL = "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces"
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const data = getAPIHeader()
+  showHeader(data)
+  
+})
+
+async function getAPIHeader() {
+  const API = "https://api.themoviedb.org/3/movie/568620?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US"
+
+  const response = await fetch(API)
+  const result = await response.json()
+
+  return result
+}
+
+function showHeader(result) {
+  result.then((e)=> {
+    document.documentElement.style.setProperty('--banner', `linear-gradient(90deg, rgba(168, 2, 2, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%), url('${bgURL + e.backdrop_path}') no-repeat`)
+  })
+}
