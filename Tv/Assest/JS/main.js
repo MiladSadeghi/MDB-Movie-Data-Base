f62c20aa30d74e52b41a653acb8a5e46acfc2fc2
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-09-05 17:59:13 +0430

    series cast

--- Tv/Assest/JS/main.js
+++ Tv/Assest/JS/main.js
@@ -9,14 +9,23 @@ const headerSpan = document.querySelector('.header span')
 const tagLine = document.querySelector('.tagline')
 const overview = document.querySelector('.movie-stuff p')
 const cast = document.querySelector('#cast')
+const seriesCastURL = 'https://www.themoviedb.org/t/p/w138_and_h175_face'
 
 document.addEventListener('DOMContentLoaded', ()=> {
   const dataHeader = getAPIHeader()
   showHeader(dataHeader)
+  const dataSeriesCast = getAPISeriesCast()
+  showMain(dataSeriesCast)
 })
 
 async function getAPIHeader() {
   const API = `https://api.themoviedb.org/3/tv/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
+  const response = await fetch(API)
+  const result = await response.json()
+  return result
+}
+async function getAPISeriesCast() {
+  const API = `https://api.themoviedb.org/3/tv/${querySearch}/credits?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
   const response = await fetch(API)
   const result = await response.json()
   return result
@@ -37,5 +46,41 @@ function showHeader(result) {
     headerSpan.innerHTML = `${e.first_air_date.replaceAll('-', '/')}  &#9679;  ${genres} &#9679; <span class="vote">${e.vote_average}</span>`
     tagLine.innerText = `"${e.tagline}"`
     overview.innerText = e.overview
+  })
+}
+
+function showMain(data) {
+  data.then((e)=> {
+    e.cast.forEach(element => {
+      if(element.order <= 10) {
+        cast.innerHTML += `
+        <div class="item cast-card">
+          <img src="${seriesCastURL + element.profile_path}">
+          <div class="cast-card-content">${element.name}</div>
+        </div>
+        `
+      }
+    });
+    cast.innerHTML += `
+      <div class="item last">view more ...</div>
+    `
+    Carousel('.owl',1,3,6)
+  })
+}
+
+function Carousel(path,responsive1,responsive2,responsive3) {
+  $(path).owlCarousel({
+    dots: true,
+    responsive:{
+        0:{
+            items:responsive1
+        },
+        600:{
+            items:responsive2
+        },
+        1000:{
+            items:responsive3
+        }
+    }
   })
 }
