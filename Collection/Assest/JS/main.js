c95f9e61d1c58aef7ce82d12835708d56c7cacdd
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-09-06 20:13:15 +0430

    Featured Crew

--- Collection/Assest/JS/main.js
+++ Collection/Assest/JS/main.js
@@ -1,115 +1,147 @@
 const search = JSON.parse(sessionStorage.getItem("collection"));
-      querySearch = search[search.length - 1]
-      bgURL = "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces"
-      posterURL = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2"
-      posterPath = document.querySelector('.img-poster img')
-      headerHead = document.querySelector('.header h1')
-      detail = document.querySelector('.detail')
-      overview = document.querySelector('.movie-stuff p')
-      movieNumber = document.querySelector('.movie-number span')
-      revenue = document.querySelector('.revenue span')
-      formatToCurrency = amount => {
-        return "$" + amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-      };
-      featuredCastContent = document.querySelector('.featured-cast-content')
-      featuredCastContentImg = 'https://www.themoviedb.org/t/p/w64_and_h64_face'
+querySearch = search[search.length - 1];
+bgURL = "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces";
+posterURL = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2";
+posterPath = document.querySelector(".img-poster img");
+headerHead = document.querySelector(".header h1");
+detail = document.querySelector(".detail");
+overview = document.querySelector(".movie-stuff p");
+movieNumber = document.querySelector(".movie-number span");
+revenue = document.querySelector(".revenue span");
+formatToCurrency = (amount) => {
+  return "$" + amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+};
+featuredSectionContentImg = "https://www.themoviedb.org/t/p/w64_and_h64_face";
+featuredCastContent = document.querySelector(".featured-cast-content");
+featuredCrewContent = document.querySelector(".featured-crew-content");
 
-document.addEventListener('DOMContentLoaded', ()=> {
-  const dataAll = getAPIAll()
-  showHeader(dataAll)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const dataAll = getAPIAll();
+  showHeader(dataAll);
+});
 
 async function getAPIAll() {
-  const API = `https://api.themoviedb.org/3/collection/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
-  const response = await fetch(API)
-  const result = await response.json()
-  return result
+  const API = `https://api.themoviedb.org/3/collection/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`;
+        response = await fetch(API);
+        result = await response.json();
+  return result;
 }
 async function getAPIkeywords() {
-  const API = `https://api.themoviedb.org/3/genre/movie/list?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
-  const response = await fetch(API)
-  const result = await response.json()
-  return result
+  const API = `https://api.themoviedb.org/3/genre/movie/list?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`;
+        response = await fetch(API);
+        result = await response.json();
+  return result;
 }
 async function getAPIRevenue(id) {
-  const API = `https://api.themoviedb.org/3/movie/${id}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
-  const response = await fetch(API)
-  const result = await response.json()
-  return result
+  const API = `https://api.themoviedb.org/3/movie/${id}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`;
+        response = await fetch(API);
+        result = await response.json();
+  return result;
 }
 async function getAPICast(id) {
-  const API = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`
-  const response = await fetch(API)
-  const result = await response.json()
-  return result
+  const API = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`;
+        response = await fetch(API);
+        result = await response.json();
+  return result;
 }
 
 function showKeywords(array) {
   let uniqueChars = array.filter((c, index) => {
     return array.indexOf(c) === index;
   });
-  let genre = '';
-  getAPIkeywords().then((e)=> {
+  let genre = "";
+  getAPIkeywords().then((e) => {
     for (let i = 0; i < e.genres.length; i++) {
       for (let j = 0; j < uniqueChars.length; j++) {
-        if(e.genres[i].id === uniqueChars[j]) {
-          genre += `${e.genres[i].name}, `
+        if (e.genres[i].id === uniqueChars[j]) {
+          genre += `${e.genres[i].name}, `;
         }
       }
     }
-    genre = genre.slice(0,-2);
-    detail.children[0].innerHTML = genre
-  })
+    genre = genre.slice(0, -2);
+    detail.children[0].innerHTML = genre;
+  });
 }
 
 function showHeader(data) {
-  let voteAverage = 0
-  let genre = []
-  let movie = []
-  data.then((e)=> {
+  let voteAverage = 0;
+      genre = [];
+      movie = [];
+  data.then((e) => {
     e.parts.forEach((element) => {
-      voteAverage += element.vote_average
-      genre.push(...element.genre_ids)
-      movie.push(element.id)
+      voteAverage += element.vote_average;
+      genre.push(...element.genre_ids);
+      movie.push(element.id);
     });
-    let cast = e.parts[e.parts.length - 1].id
-    showFeaturedCast(cast)
-    showKeywords(genre)
-    showRevenue(movie)
-    document.documentElement.style.setProperty('--banner', `linear-gradient(90deg, rgba(168, 2, 2, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%), url('${bgURL + e.backdrop_path}') no-repeat`)
-    posterPath.src = posterURL + e.poster_path
-    headerHead.innerText = e.name
-    document.title = e.name + ' - Im Second IMDB!'
-    detail.innerHTML += `<span class="genre"></span>  	&#9679;  <span class="vote">${voteAverage / e.parts.length}</span>`
-    overview.innerText = e.overview
-    movieNumber.innerHTML = `${e.parts.length}`
-  })
+    let cast = e.parts[e.parts.length - 1].id;
+    showFeaturedSection(cast);
+    showKeywords(genre);
+    showRevenue(movie);
+    document.documentElement.style.setProperty("--banner", `linear-gradient(90deg, rgba(168, 2, 2, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%), url('${bgURL + e.backdrop_path}') no-repeat`);
+    posterPath.src = posterURL + e.poster_path;
+    headerHead.innerText = e.name;
+    document.title = e.name + " - Im Second IMDB!";
+    detail.innerHTML += `<span class="genre"></span>  	&#9679;  <span class="vote">${voteAverage / e.parts.length}</span>`;
+    overview.innerText = e.overview;
+    movieNumber.innerHTML = `${e.parts.length}`;
+  });
 }
 
 function showRevenue(id) {
-  let revenueMoney = 0
+  let revenueMoney = 0;
   for (const iterator of id) {
-    getAPIRevenue(iterator).then((e)=> {
-      revenueMoney += e.revenue
-      revenue.innerHTML = `${formatToCurrency(revenueMoney)}`
-    })
+    getAPIRevenue(iterator).then((e) => {
+      revenueMoney += e.revenue;
+      revenue.innerHTML = `${formatToCurrency(revenueMoney)}`;
+    });
   }
 }
 
-function showFeaturedCast(cast) {
-  getAPICast(cast).then((e)=> {
-    e.cast.forEach(element => {
-      if(element.order < 14) {
+function showFeaturedSection(cast) {
+  getAPICast(cast).then((e) => {
+    e.cast.forEach((element) => {
+      if (element.order < 14) {
         featuredCastContent.innerHTML += `
-        <div class="card">
-          <img src="${featuredCastContentImg + element.profile_path}">
-          <div>
-            <h5>${element.name}</h5>
-            <p>${element.character}</p>
-          </div>
-        </div>
-        `
+          <div class="card">
+            <img src="${featuredSectionContentImg + element.profile_path}">
+            <div>
+              <h5>${element.name}</h5>
+              <p>${element.character}</p>
+            </div>
+          </div>`;
       }
     });
-  })
+    e.crew.forEach((element) => {
+      let loopCounter = 0;
+      if (loopCounter < 3) {
+        if (element.department === "Directing" || element.department === "Writing") {
+          if (featuredCrewContent.innerHTML.trim()) {
+            if (element.name === featuredCrewContent.children[loopCounter].children[1].children[0].textContent) {
+              featuredCrewContent.children[loopCounter].children[1].children[1].textContent += `, ${element.department}`;
+            } else {
+              featuredCrewContent.innerHTML += `
+                <div class="card">
+                  <img src="${featuredSectionContentImg + element.profile_path}">
+                  <div>
+                    <h5>${element.name}</h5>
+                    <p>${element.department}</p>
+                  </div>
+                </div>`;
+              loopCounter++;
+            }
+          } else {
+            featuredCrewContent.innerHTML += `
+                <div class="card">
+                  <img src="${featuredSectionContentImg + element.profile_path}">
+                  <div>
+                    <h5>${element.name}</h5>
+                    <p>${element.department}</p>
+                  </div>
+                </div>`;
+              loopCounter++;
+          }
+        }
+      }
+    });
+  });
 }
