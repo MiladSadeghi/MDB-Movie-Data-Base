baf885375e15f5f362e2975a2934d2ba49f1af6e
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-09-09 20:24:40 +0430

    Company Information (company)

--- Company/Assest/JS/main.js
+++ Company/Assest/JS/main.js
@@ -1,14 +1,29 @@
+const search = JSON.parse(sessionStorage.getItem("company"));
+querySearch = search[search.length - 1]
+
 const companyContent = document.querySelector('.company-content'),
-companyMovieImgURL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2'
+companyMovieImgURL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2',
+companyName = document.querySelector('#company-name'),
+headQuarters = document.querySelector('#headquarters'),
+country = document.querySelector('#country'),
+movieCounter = document.querySelector('#movie-counter')
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dataMain = getAPICompany()
-  showContent(dataMain)
+  const dataMainContent = getAPICompanyContent()
+  showContent(dataMainContent)
+  const dataMainCompany = getAPICompany()
+  showCompany(dataMainCompany)
 })
 
 async function getAPICompany() {
-  const API = 'https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=15935',
+  const API = `https://api.themoviedb.org/3/company/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c`,
+  response = await fetch(API),
+  result = await response.json()
+  return result
+}
+async function getAPICompanyContent() {
+  const API = `https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=${querySearch}`,
   response = await fetch(API),
   result = await response.json()
   return result
@@ -16,6 +31,7 @@ async function getAPICompany() {
 
 function showContent(result) {
   result.then(e => {
+    movieCounter.innerHTML = e.results.length + ' Movie'
     e.results.forEach(element => {
       companyContent.innerHTML += `
       <div class="row">
@@ -30,5 +46,14 @@ function showContent(result) {
       </div>
       `
     });
+  })
+}
+
+function showCompany(result) {
+  result.then(e => {
+    document.title = e.name + ' - Use Me As Second IMDB!'
+    companyName.innerHTML += e.name
+    headQuarters.innerHTML += e.headquarters
+    country.innerHTML += e.origin_country
   })
 }
