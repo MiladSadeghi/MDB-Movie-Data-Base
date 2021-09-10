9b42f7393aded2b91c5193fe8f6300ff794920cf
MiladSadeghi <miladsadeghi2323@gmail.com>
2021-09-10 13:08:30 +0430

    keywrod head (keyword)

--- Keyword/Assest/JS/main.js
+++ Keyword/Assest/JS/main.js
@@ -1,16 +1,25 @@
+const search = JSON.parse(sessionStorage.getItem("keyword"));
+querySearch = search[search.length - 1]
+
 const keywordName = document.querySelector('#keyword-name'),
 keywordContent = document.querySelector('.keyword-content'),
 keywordMovieImgURL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2'
 
-
 document.addEventListener('DOMContentLoaded', ()=> {
   const dataKeyword = getAPIKeyword()
   showMain(dataKeyword)
+  const dataKeywordName = getAPIKeywordName()
+  showKeywordName(dataKeywordName)
 })
 
-
 async function getAPIKeyword() {
-  const API = 'https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&with_keywords=262443',
+  const API = `https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&with_keywords=${querySearch}`,
+  response = await fetch(API),
+  result = await response.json()
+  return result
+}
+async function getAPIKeywordName() {
+  const API = `https://api.themoviedb.org/3/keyword/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c`,
   response = await fetch(API),
   result = await response.json()
   return result
@@ -18,6 +27,10 @@ async function getAPIKeyword() {
 
 function showMain(result) {
   result.then(e => {
+    if(e.results.length === 0) {
+      keywordContent.innerHTML = `<h3>Nothing Found</h3>`
+      return true
+    }
     e.results.forEach(element => {
       keywordContent.innerHTML += `
       <div class="row">
@@ -32,5 +45,12 @@ function showMain(result) {
     </div>
     `
     });
+  })
+}
+
+function showKeywordName(result) {
+  result.then(e => {
+    document.title = e.name + ' - Use Me As Second IMDB!'
+    keywordName.innerHTML = e.name
   })
 }
