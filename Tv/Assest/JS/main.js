42fdb86d71cdaeb133af96432c1eeb75917ea77e
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-05-19 20:42:51 +0430

    Refactor - TV - Fact Side

--- Tv/Assest/JS/main.js
+++ Tv/Assest/JS/main.js
@@ -7,6 +7,13 @@ const runTime = document.querySelector(".runtime");
 const tagLine = document.querySelector(".tagline");
 const overview = document.querySelector(".overview");
 const headerCrew = document.querySelector(".header-crew");
+const socialIcon = document.querySelector(".social-icon");
+const sideFact = document.querySelector(".facts");
+const statusFact = document.querySelector(".status");
+const languageFact = document.querySelector(".language");
+const networkFact = document.querySelector(".network");
+const typeFact = document.querySelector(".type");
+const keywordFact = document.querySelector(".keyword");
 const movieIDParam = new URLSearchParams(location.search).get("id");
 const headerSection = document.querySelector(".header");
 const poster = document.querySelector(".image-banner img")
@@ -14,6 +21,7 @@ const bannerImageURL = "https://image.tmdb.org/t/p/original"
 const posterImageURL = "https://www.themoviedb.org/t/p/original"
 const castImageURL = "https://www.themoviedb.org/t/p/original"
 const recommendImageURL = "https://www.themoviedb.org/t/p/original"
+const networkImageURL = "https://www.themoviedb.org/t/p/h30"
 
 let tvDataObj = {}
 let apiURL = [`https://api.themoviedb.org/3/tv/${movieIDParam}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`, `https://api.themoviedb.org/3/tv/${movieIDParam}/content_ratings?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`, `https://api.themoviedb.org/3/tv/${movieIDParam}/credits?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`, `https://api.themoviedb.org/3/tv/${movieIDParam}/external_ids?api_key=75c8aed355937ba0502f74d9a1aed11c`, `https://api.themoviedb.org/3/tv/${movieIDParam}/keywords?api_key=75c8aed355937ba0502f74d9a1aed11c`, `https://api.themoviedb.org/3/tv/${movieIDParam}/recommendations?api_key=75c8aed355937ba0502f74d9a1aed11c`, `https://api.themoviedb.org/3/tv/${movieIDParam}/videos?api_key=75c8aed355937ba0502f74d9a1aed11c`, `https://api.themoviedb.org/3/tv/${movieIDParam}/images?api_key=75c8aed355937ba0502f74d9a1aed11c`]
@@ -31,7 +39,12 @@ function getFromAPI(apiURL) {
     tvDataObj["tvDetails"] = datas[0];
     tvDataObj["contentRating"] = datas[1];
     tvDataObj["Cast&Crew"] = datas[2];
+    tvDataObj["socialMedia"] = datas[3];
+    tvDataObj["keywords"] = datas[4];
     header();
+    aside();
+
+    createToolTip()
   })
 }
 
@@ -73,6 +86,55 @@ function arrangeHeaderPeople() {
   return importantCrew
 }
 
+function availableSocialMedia() {
+  let social = [];
+  if (tvDataObj["socialMedia"].facebook_id !== undefined) {
+    social.push(`<a data-bs-toggle="tooltip" data-bs-html="true" title="Visit Facebook" href="https://www.facebook.com/${tvDataObj["socialMedia"].facebook_id}" target="_blank"><i class="bi bi-facebook"></i></a>`)
+  }
+  if (tvDataObj["socialMedia"].instagram_id !== undefined) {
+    social.push(`<a data-bs-toggle="tooltip" data-bs-html="true" title="Visit Instagram" href="https://www.instagram.com/${tvDataObj["socialMedia"].instagram_id}" target="_blank"><i class="bi bi-instagram"></i></a>`)
+  }
+  if (tvDataObj["socialMedia"].twitter_id !== undefined) {
+    social.push(`<a data-bs-toggle="tooltip" data-bs-html="true" title="Visit Twiter" href="https://www.twitter.com/${tvDataObj["socialMedia"].twitter_id}" target="_blank"><i class="bi bi-twitter"></i></a>`)
+  }
+  if (tvDataObj["tvDetails"].homepage !== undefined) {
+    social.push(`<a data-bs-toggle="tooltip" data-bs-html="true" title="Visit Homepage" href="${tvDataObj["tvDetails"].homepage}" target="_blank"><i class="fs-4 bi bi-link"></i></a>`)
+  }
+  return social
+}
+
+function getTvLanguage() {
+  let language = tvDataObj["tvDetails"].spoken_languages.map((item) => {
+    if (item.iso_639_1 === tvDataObj["tvDetails"].original_language) {
+      return item.english_name;
+    }
+  })
+  return language.join("");
+}
+
+function getKeywords() {
+  let keywords = [];
+  tvDataObj["keywords"].results.filter((item) => {
+    keywords.push(`<a href="#" class="badge bg-secondary text-white me-2 my-1" target="_blank">${item.name}</a>`)
+  })
+  return keywords.join("");
+}
+
+function getNetwork() {
+  let network = [];
+  tvDataObj["tvDetails"].networks.filter((item) => {
+    network.push(`<div><img data-bs-toggle="tooltip" data-bs-html="true" title="${item.name}" data-bs-placement="right" src="${networkImageURL+item.logo_path}"></div>`)
+  })
+  return network.join("");
+}
+
+function createToolTip() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+}
+
 function header() {
   document.title = tvDataObj["tvDetails"].original_name + ' - IMDB #2';
   poster.src = `${posterImageURL}${tvDataObj["tvDetails"].poster_path}`;
@@ -88,4 +150,13 @@ function header() {
   tagLine.innerHTML = `${tvDataObj["tvDetails"].tagline}`
   overview.innerHTML = `${tvDataObj["tvDetails"].overview}`
   headerCrew.insertAdjacentHTML("beforeend", arrangeHeaderPeople().join(""));
+}
+
+function aside() {
+  socialIcon.insertAdjacentHTML("beforeend", availableSocialMedia().join(""));
+  statusFact.innerHTML = tvDataObj["tvDetails"].status;
+  languageFact.innerHTML = getTvLanguage();
+  networkFact.innerHTML = getNetwork();
+  typeFact.innerHTML = tvDataObj["tvDetails"].type;
+  keywordFact.innerHTML = getKeywords();
 }
