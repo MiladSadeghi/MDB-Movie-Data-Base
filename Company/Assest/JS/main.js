4b0408631690f8964859e67b21a226e69b69176b
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-05-25 21:14:11 +0430

    Refactor - Company - Header

--- Company/Assest/JS/main.js
+++ Company/Assest/JS/main.js
@@ -1,76 +1,29 @@
-const search = JSON.parse(sessionStorage.getItem("company"));
-querySearch = search[search.length - 1]
-
-const companyContent = document.querySelector('.company-content'),
-companyMovieImgURL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2',
-companyName = document.querySelector('#company-name'),
-headQuarters = document.querySelector('#headquarters'),
-country = document.querySelector('#country'),
-movieCounter = document.querySelector('#movie-counter')
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const dataMainContent = getAPICompanyContent()
-  showContent(dataMainContent)
-  const dataMainCompany = getAPICompany()
-  showCompany(dataMainCompany)
+const companyLogo = document.querySelector("#company-logo");
+const contentCounter = document.querySelector("#data-counter");
+const informationBar = document.querySelector(".information")
+const companyIDParam = new URLSearchParams(location.search).get("id");
+const companyIDParam2 = new URLSearchParams(location.search).get("show");
+const originalImageURL = "https://image.tmdb.org/t/p/original";
+let apiURL = [`https://api.themoviedb.org/3/company/${companyIDParam}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`, `https://api.themoviedb.org/3/discover/${companyIDParam2}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=${companyIDParam}`];
+let companyDataObj = {};
+document.addEventListener("DOMContentLoaded", () => {
+  getFromAPI(apiURL);
 })
 
-async function getAPICompany() {
-  const API = `https://api.themoviedb.org/3/company/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c`,
-  response = await fetch(API),
-  result = await response.json()
-  return result
-}
-async function getAPICompanyContent() {
-  const API = `https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=${querySearch}`,
-  response = await fetch(API),
-  result = await response.json()
-  return result
-}
-
-function showContent(result) {
-  result.then(e => {
-    movieCounter.innerHTML = e.results.length + ' Movie'
-    e.results.forEach(element => {
-      companyContent.innerHTML += `
-      <a href="#" class="move" data-id="${element.id}">
-        <div class="row" data-id="${element.id}">
-          <img class="${(element.poster_path)? '': 'bg'}" src="${(element.poster_path)? companyMovieImgURL + element.poster_path: 'Assest/Images/loadingImage.png'}" data-id="${element.id}">
-          <div class="row-content" data-id="${element.id}">
-            <div data-id="${element.id}">
-              <h2 data-id="${element.id}">${element.original_title}</h2>
-              <span data-id="${element.id}">${element.release_date.replaceAll('-','/')}</span>
-            </div>
-            <p data-id="${element.id}">${element.overview}</p>
-          </div>
-        </div>
-      </a>
-      `
-    });
-    moveToPage('movie', 'Movie') 
+async function getFromAPI(apiURL) {
+  let requests = apiURL.map(async (item) =>
+    await fetch(item).then(async (response) => await response.json()));
+  Promise.all(requests).then(async (datas) => {
+    companyDataObj["company"] = await datas[0];
+    companyDataObj["companyMovie"] = await datas[1];
+    header();
+    console.log(companyDataObj);
   })
 }
 
-function showCompany(result) {
-  result.then(e => {
-    document.title = e.name + ' - Use Me As Second IMDB!'
-    companyName.innerHTML += e.name
-    headQuarters.innerHTML += e.headquarters
-    country.innerHTML += e.origin_country
-  })
-}
-
-function moveToPage(storage, path) {
-  let lists = []
-  const move = document.querySelectorAll('.move')
-  move.forEach(element => {
-    element.addEventListener('click',(e)=> {
-      e.preventDefault()
-      console.log(e.target);
-      lists.push(e.target.getAttribute('data-id'))
-      sessionStorage.setItem(storage, JSON.stringify(lists))
-      window.open(`../${path}`, '_blank')
-    })
-  });
+function header() {
+  let counter = String(companyDataObj["companyMovie"].total_results).split("").reverse().join("").match(/.{1,3}/g).reverse().join(",");
+  companyLogo.src = `${originalImageURL}${companyDataObj["company"].logo_path}`
+  contentCounter.innerHTML = `${counter} ${companyIDParam2}`
+  informationBar.innerHTML = `<div><i class="bi bi-building fs-5"></i> ${companyDataObj["company"].name}</div><div><i class="bi bi-geo-alt-fill fs-5"></i> ${companyDataObj["company"].headquarters}</div><div><i class="bi bi-globe2 fs-5"></i> ${companyDataObj["company"].origin_country}</div><div><i class="bi bi-link fs-5"></i> <a href="${companyDataObj["company"].homepage}" class="text-color">Homepage</a></div>`
 }
