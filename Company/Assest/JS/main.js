b6b8af28aefb62077130fe996ef011ae755a163a
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-05-25 22:37:41 +0430

    Refactor - Company - PageNation

--- Company/Assest/JS/main.js
+++ Company/Assest/JS/main.js
@@ -1,26 +1,61 @@
 const companyLogo = document.querySelector("#company-logo");
 const contentCounter = document.querySelector("#data-counter");
-const informationBar = document.querySelector(".information")
-const bodyContent = document.querySelector(".body-content")
+const informationBar = document.querySelector(".information");
+const bodyContent = document.querySelector(".body-content");
+const pageNationUlist = document.querySelector("#ul-pagenation");
 const companyIDParam = new URLSearchParams(location.search).get("id");
 const companyIDParam2 = new URLSearchParams(location.search).get("show");
 const originalImageURL = "https://image.tmdb.org/t/p/original";
 let apiURL = [`https://api.themoviedb.org/3/company/${companyIDParam}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US`, `https://api.themoviedb.org/3/discover/${companyIDParam2}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=${companyIDParam}`];
-let companyDataObj = {};
+let companyDataObj = {page: 1};
 document.addEventListener("DOMContentLoaded", () => {
-  getFromAPI(apiURL);
+  getAllFromAPI(apiURL);
+  pageNationUlist.addEventListener('click', async (e) => {
+    e.stopImmediatePropagation();
+    if (e.target.classList.contains("page-next")) {
+      if (companyDataObj["companyMovie"].page <= companyDataObj["companyMovie"].total_results) {
+        companyDataObj["page"] += 1;
+      }
+      await pageNation(companyDataObj["page"], companyDataObj["companyMovie"].total_results);
+      body();
+    }
+    if (e.target.classList.contains("page-prev")) {
+      if (companyDataObj["companyMovie"].page <= companyDataObj["companyMovie"].total_results) {
+        companyDataObj["page"] -= 1;
+      }
+      await pageNation(companyDataObj["page"], companyDataObj["companyMovie"].total_results);
+      body();
+    }
+  })
 })
 
-async function getFromAPI(apiURL) {
+async function getAllFromAPI(apiURL) {
   let requests = apiURL.map(async (item) =>
     await fetch(item).then(async (response) => await response.json()));
   Promise.all(requests).then(async (datas) => {
     companyDataObj["company"] = await datas[0];
+    document.title = `${companyDataObj["company"].name} - #2 IMDB`;
     companyDataObj["companyMovie"] = await datas[1];
     header();
     body();
     console.log(companyDataObj);
   })
+}
+async function getFromAPI(API_LINK) {
+  const response = await fetch(API_LINK);
+  const result = await response.json();
+  return result;
+}
+
+async function pageNation(pageNow, totalPage) {
+  let api = `https://api.themoviedb.org/3/discover/${companyIDParam2}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_companies=${companyIDParam}&page=${pageNow}`;
+  let result = await getFromAPI(api)
+  companyDataObj["companyMovie"] = await result;
+  console.log(companyDataObj);
+  pageNationUlist.innerHTML = `
+  <li class="page-item ${pageNow === 1 ? "disabled" : ""}"><a class="page-link page-prev" href="#">Previous</a></li>
+  <li class="page-item ${pageNow === totalPage ? 'disabled' : ''} "><a class="page-link page-next" href="#">Next</a></li>
+  `
 }
 
 function header() {
@@ -32,26 +67,27 @@ function header() {
 
 function body() {
   let content = [];
-  
+  bodyContent.innerHTML = "";
   companyDataObj["companyMovie"].results.forEach((item) => {
     let movieDate = new Date(item.release_date);
-    movieDate = `${movieDate.toLocaleString([], {month: 'long'})} ${movieDate.getDay()}, ${movieDate.getFullYear()}`
+    movieDate = `${movieDate.toLocaleString([], { month: 'long' })} ${movieDate.getDay()}, ${movieDate.getFullYear()}`
     content.push(`
     <div class="card mb-4">
-  <div class="row g-0">
-    <div class="col-md-2">
-      <img src="${originalImageURL}${item.poster_path}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-10">
-      <div class="card-body">
-        <h5 class="card-title text-color">${item.original_title}</h5>
-        <p class="card-text fs-6 text-color">${movieDate}</p>
-        <p class="card-text fs-6 text-color">${item.overview}</p>
+      <div class="row g-0">
+        <div class="col-md-2">
+          <img src="${originalImageURL}${item.poster_path}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-10">
+          <div class="card-body">
+            <h5 class="card-title text-color">${item.original_title}</h5>
+            <p class="card-text fs-6 text-color">${movieDate}</p>
+            <p class="card-text fs-6 text-color">${item.overview}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
     `)
   })
   bodyContent.insertAdjacentHTML("beforeend", content.join(""));
+  pageNation(companyDataObj["companyMovie"].page, companyDataObj["companyMovie"].total_pages);
 }
