2ee7d9acdf1d7ba708a6fd6206fd6bb270d96ea0
MiladSadeghi <miladsadeghi2323@gmail.com>
2022-05-26 19:43:41 +0430

    Refactor - Keyword - Header

--- Keyword/Assest/JS/main.js
+++ Keyword/Assest/JS/main.js
@@ -1,72 +1,29 @@
-const search = JSON.parse(sessionStorage.getItem("keyword"));
-querySearch = search[search.length - 1]
+const keyword = document.querySelector("#keyword");
+const contentCounter = document.querySelector("#data-counter");
+const windowLocationParams = new URLSearchParams(location.search)
+const keywordIDParam = windowLocationParams.get("id");
+const keywordIDParam2 = windowLocationParams.get("show");
+const keywordIDParam3 = windowLocationParams.get("keyword");
 
-const keywordName = document.querySelector('#keyword-name'),
-keywordContent = document.querySelector('.keyword-content'),
-keywordMovieImgURL = 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2'
+let keywordDataObj = {};
+let apiURL = [`https://api.themoviedb.org/3/discover/${keywordIDParam2}?api_key=75c8aed355937ba0502f74d9a1aed11c&language=en-US&with_keywords=${keywordIDParam}`];
 
-document.addEventListener('DOMContentLoaded', ()=> {
-  const dataKeyword = getAPIKeyword()
-  showMain(dataKeyword)
-  const dataKeywordName = getAPIKeywordName()
-  showKeywordName(dataKeywordName)
+document.addEventListener("DOMContentLoaded", () => {
+  getAllFromAPI(apiURL);
 })
 
-async function getAPIKeyword() {
-  const API = `https://api.themoviedb.org/3/discover/movie?api_key=75c8aed355937ba0502f74d9a1aed11c&with_keywords=${querySearch}`,
-  response = await fetch(API),
-  result = await response.json()
-  return result
-}
-async function getAPIKeywordName() {
-  const API = `https://api.themoviedb.org/3/keyword/${querySearch}?api_key=75c8aed355937ba0502f74d9a1aed11c`,
-  response = await fetch(API),
-  result = await response.json()
-  return result
-}
-
-function showMain(result) {
-  result.then(e => {
-    if(e.results.length === 0) {
-      keywordContent.innerHTML = `<h3>Nothing Found</h3>`
-      return true
-    }
-    e.results.forEach(element => {
-      keywordContent.innerHTML += `
-      <a href="#" class="move" data-id="${element.id}">
-        <div class="row" data-id="${element.id}">
-          <img class="${(element.poster_path)? '': 'bg'}" src="${(element.poster_path)? keywordMovieImgURL + element.poster_path: 'Assest/Images/loadingImage.png'}" data-id="${element.id}">
-          <div class="row-content" data-id="${element.id}">
-            <div data-id="${element.id}">
-              <h2 data-id="${element.id}">${element.original_title}</h2>
-              <span data-id="${element.id}">${element.release_date.replaceAll('-','/')}</span>
-            </div>
-            <p data-id="${element.id}">${element.overview}</p>
-          </div>
-        </div>
-      </a>
-      `
-    });
-    moveToPage('movie', 'Movie')
+async function getAllFromAPI(apiURL) {
+  let requests = apiURL.map(async (item) =>
+    await fetch(item).then(async (response) => await response.json()));
+  Promise.all(requests).then(async (datas) => {
+    document.title = `${keywordIDParam3} - #2 IMDB`;
+    keywordDataObj["keywordContent"] = await datas[0];
+    header();
   })
 }
 
-function showKeywordName(result) {
-  result.then(e => {
-    document.title = e.name + ' - Use Me As Second IMDB!'
-    keywordName.innerHTML = e.name
-  })
-}
-
-function moveToPage(storage, path) {
-  let lists = []
-  const move = document.querySelectorAll('.move')
-  move.forEach(element => {
-    element.addEventListener('click',(e)=> {
-      e.preventDefault()
-      lists.push(e.target.getAttribute('data-id'))
-      sessionStorage.setItem(storage, JSON.stringify(lists))
-      window.open(`../${path}`, '_blank')
-    })
-  });
+function header() {
+  let counter = String(keywordDataObj["keywordContent"].total_results).split("").reverse().join("").match(/.{1,3}/g).reverse().join(",");
+  contentCounter.innerHTML = `${counter} ${keywordIDParam2}`
+  keyword.innerHTML = `${keywordIDParam3}`
 }
